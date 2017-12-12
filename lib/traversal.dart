16de5e482e2b0bb1a0989cb1a9a1c26b57e29625
Greg Spencer <gspencer@google.com>
2017-12-11 16:21:23 -0800

    Adding traversal target

---
+++ lib/traversal.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Accessibility Traversal Test Code',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Accessibility Traversal Test'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static bool multiLayer = false;
+
+  Widget _makeMultilayerCell(BuildContext context, int row, int col) {
+    return new Semantics(
+      sortKey: new GroupSortKey(col.toDouble()),
+      child: new Semantics(
+        sortKey: new LinearSortKey(row.toDouble()),
+        label: '$row, $col',
+        child: new ExcludeSemantics(
+          child: new ConstrainedBox(
+            constraints: new BoxConstraints.tight(const Size.fromRadius(50.0)),
+            child: new Center(
+              child: new Text('$row, $col', style: Theme.of(context).textTheme.display1),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _makeFlatCell(BuildContext context, int row, int col) {
+    return new Semantics(
+      sortOrder: new SemanticsSortOrder(
+        keys: <SemanticsSortKey>[
+          new GroupSortKey(col.toDouble()),
+          new LinearSortKey(row.toDouble())
+        ],
+      ),
+      label: '$row, $col',
+      child: new ExcludeSemantics(
+        child: new ConstrainedBox(
+          constraints: new BoxConstraints.tight(const Size.fromRadius(50.0)),
+          child: new Center(
+            child: new Text('$row, $col', style: Theme.of(context).textTheme.display1),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _makeCell(BuildContext context, int row, int col) {
+    return multiLayer ? _makeMultilayerCell(context, row, col) : _makeFlatCell(context, row, col);
+  }
+
+  Widget _makeCenterCell(BuildContext context) {
+    if (multiLayer) {
+      return new Semantics(
+        sortKey: new GroupSortKey(1.0),
+        child: new Semantics(
+          sortKey: new LinearSortKey(3.0),
+          label: 'Center',
+          child: new ExcludeSemantics(
+            child: _makeMultilayerCell(context, 1, 1),
+          ),
+        ),
+      );
+    } else {
+      return new Semantics(
+        sortOrder: new SemanticsSortOrder(
+          keys: <SemanticsSortKey>[new GroupSortKey(1.0), new LinearSortKey(3.0)],
+        ),
+        label: "Center",
+        child: new ExcludeSemantics(
+          child: _makeCell(context, 1, 1),
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      theme: new ThemeData.light(),
+      home: new Scaffold(
+        body: new Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _makeCell(context, 0, 0),
+                _makeCell(context, 0, 1),
+                _makeCell(context, 0, 2),
+              ],
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _makeCell(context, 1, 0),
+                _makeCenterCell(context),
+                _makeCell(context, 1, 2),
+              ],
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _makeCell(context, 2, 0),
+                _makeCell(context, 2, 1),
+                _makeCell(context, 2, 2),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
